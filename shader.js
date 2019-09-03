52f852824c6f40ea09172a8e78ae89c6987436ee
Dylan Babbs <dylan.babbs@here.com>
2019-09-02 19:40:06 -0700

    play around with noise

--- shader.js
+++ shader.js
@@ -2,6 +2,7 @@ const canvasSketch = require('canvas-sketch');
 const createShader = require('canvas-sketch-util/shader');
 const glsl = require('glslify');
 
+
 // Setup our sketch
 const settings = {
    context: 'webgl',
@@ -20,17 +21,23 @@ const frag = glsl( /* glsl */ `
    uniform float aspect;
    varying vec2 vUv;
 
+   #pragma glslify: noise = require('glsl-noise/simplex/3d');
+
    void main () {
-      vec3 colorA = cos(time) + vec3(1.0, 0.0, 0.0);
-      vec3 colorB = vec3(0.0, 0.0, 1.0);
+      // vec3 colorA = cos(time) + vec3(1.0, 0.0, 0.0);
+      // vec3 colorB = vec3(0.0, 0.0, 1.0);
 
-      vec2 center = vUv - 0.5;
-      center.x *= aspect;
-      float dist = length(center);
-      float alpha = smoothstep(0.2515, 0.25, dist);
+      // vec2 center = vUv - 0.5;
+      // center.x *= aspect;
+      // float dist = length(center);
+      // float alpha = smoothstep(0.2515, 0.25, dist);
 
-      vec3 color = mix(colorA, colorB, vUv.y + vUv.x * sin(time));
-      gl_FragColor = vec4(color, alpha);
+      // vec3 color = mix(colorA, colorB, vUv.y + vUv.x * sin(time));
+      // gl_FragColor = vec4(color, alpha);
+
+      float n = noise(vec3(vUv.xy * 2.0, time));
+
+      gl_FragColor = vec4(vec3(n), 1.0);
    }
 `);
 
