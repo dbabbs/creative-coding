276f134bea902fcd27fef21bb78fef5074b21d20
Dylan Babbs <dylan.babbs@here.com>
2019-09-02 19:47:49 -0700

    tiedye

--- shader.js
+++ shader.js
@@ -22,22 +22,29 @@ const frag = glsl( /* glsl */ `
    varying vec2 vUv;
 
    #pragma glslify: noise = require('glsl-noise/simplex/3d');
+   #pragma glslify: hsl2rgb = require('glsl-hsl2rgb');
 
    void main () {
       // vec3 colorA = cos(time) + vec3(1.0, 0.0, 0.0);
       // vec3 colorB = vec3(0.0, 0.0, 1.0);
 
-      // vec2 center = vUv - 0.5;
-      // center.x *= aspect;
+      vec2 center = vUv - 0.5;
+      center.x *= aspect;
       // float dist = length(center);
       // float alpha = smoothstep(0.2515, 0.25, dist);
 
       // vec3 color = mix(colorA, colorB, vUv.y + vUv.x * sin(time));
       // gl_FragColor = vec4(color, alpha);
 
-      float n = noise(vec3(vUv.xy * 2.0, time));
+      float n = noise(vec3(center * 3.0, time / 1.5));
 
-      gl_FragColor = vec4(vec3(n), 1.0);
+      vec3 color = hsl2rgb(
+         n,
+         0.5,
+         0.5
+      );
+
+      gl_FragColor = vec4(color, 1.0);
    }
 `);
 
