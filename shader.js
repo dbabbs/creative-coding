ad54290ffd85a2c686349af20caf08caf482ae81
Dylan Babbs <dylan.babbs@here.com>
2019-09-02 19:16:43 -0700

    awesome circle

--- shader.js
+++ shader.js
@@ -4,37 +4,52 @@ const glsl = require('glslify');
 
 // Setup our sketch
 const settings = {
-  context: 'webgl',
-  animate: true
+   context: 'webgl',
+   animate: true
 };
+/*
+    vec3 color = 0.5 + 0.5 * cos(time + vUv.xyx + vec3(0.0, 2.0, 4.0));
+    gl_FragColor = vec4(color, 1.0);
+*/
 
 // Your glsl code
 const frag = glsl( /* glsl */ `
-  precision highp float;
+   precision highp float;
 
-  uniform float time;
-  varying vec2 vUv;
+   uniform float time;
+   uniform float aspect;
+   varying vec2 vUv;
 
-  void main () {
-    vec3 color = 0.5 + 0.5 * cos(time + vUv.xyx + vec3(0.0, 2.0, 4.0));
-    gl_FragColor = vec4(color, 1.0);
-  }
+   void main () {
+      vec3 colorA = cos(time) + vec3(1.0, 0.0, 0.0);
+      vec3 colorB = vec3(0.0, 0.0, 1.0);
+
+      vec2 center = vUv - 0.5;
+      center.x *= aspect;
+      float dist = length(center);
+      float alpha = smoothstep(0.2515, 0.25, dist);
+
+      vec3 color = mix(colorA, colorB, vUv.y + vUv.x * sin(time));
+      gl_FragColor = vec4(color, alpha);
+   }
 `);
 
 // Your sketch, which simply returns the shader
 const sketch = ({ gl }) => {
-  // Create the shader and return it
-  return createShader({
-    // Pass along WebGL context
-    gl,
-    // Specify fragment and/or vertex shader strings
-    frag,
-    // Specify additional uniforms to pass down to the shaders
-    uniforms: {
+   // Create the shader and return it
+   return createShader({
+      // clearColor: false,
+      // Pass along WebGL context
+      gl,
+      // Specify fragment and/or vertex shader strings
+      frag,
+      // Specify additional uniforms to pass down to the shaders
+      uniforms: {
       // Expose props from canvas-sketch
-      time: ({ time }) => time
-    }
-  });
+         time: ({ time }) => time,
+         aspect: ({ width, height }) => width / height
+      }
+   });
 };
 
 canvasSketch(sketch, settings);
