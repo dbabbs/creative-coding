82266719bdc1eec2ae5aed6eea74bd10d116192f
Dylan Babbs <dylan.babbs@here.com>
2019-09-11 10:15:07 -0700

    add progress

--- shader.js
+++ shader.js
@@ -6,7 +6,9 @@ const glsl = require('glslify');
 // Setup our sketch
 const settings = {
    context: 'webgl',
-   animate: true
+   animate: true,
+   dimensions: [ 512, 512 ],
+   duration: 4
 };
 /*
     vec3 color = 0.5 + 0.5 * cos(time + vUv.xyx + vec3(0.0, 2.0, 4.0));
@@ -30,21 +32,21 @@ const frag = glsl( /* glsl */ `
 
       vec2 center = vUv - 0.5;
       center.x *= aspect;
-      // float dist = length(center);
-      // float alpha = smoothstep(0.2515, 0.25, dist);
+      float dist = length(center);
+      float alpha = smoothstep(0.25, 0.235, dist);
 
       // vec3 color = mix(colorA, colorB, vUv.y + vUv.x * sin(time));
       // gl_FragColor = vec4(color, alpha);
 
-      float n = noise(vec3(center * 3.0, time / 1.5));
+      float n = noise(vec3(center * 0.5, time * 0.5));
 
       vec3 color = hsl2rgb(
-         n,
+         0.7 + n * 0.2,
          0.5,
          0.5
       );
 
-      gl_FragColor = vec4(color, 1.0);
+      gl_FragColor = vec4(color, alpha);
    }
 `);
 
